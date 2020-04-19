1235c74f85cf96d202581946dc38a59b716838a8
Alexander Hinton <ahinton1991@gmail.com>
2020-04-19 19:07:01 -0400

    updating notebook and functions

--- src/utils.R
+++ src/utils.R
@@ -141,3 +141,116 @@ race_update <- function(glicko_obj, rankings_df, race_df, ovo_df, c){
   
   list(glicko, rankings_df, correlation, r_squared)
 }
+
+read_races <- function(calendar, gender){
+  # define years and make empty list to hold races
+  years <- names(calendar)
+  races <- list()
+  # iterate through calendar, download race and ovo race 
+  for (i in seq_along(calendar)){
+    year <- as.numeric(years[i])
+    series <- calendar[[i]]
+    for (race in series){
+      race_df <- read_csv(paste0("../data/",year,"_races/races_clean/",race,"_",gender,".csv"), 
+                          col_types = cols())
+      ovo_df <- read_csv(paste0("../data/",year,"_races/ovo_races/",race,"_",gender,".csv"),
+                         col_types = cols())
+      # format all races as list of lists for use in functions
+      race_tuple <- list(race_df, ovo_df)
+      # add to list of races
+      races[[length(races)+1]] <-  race_tuple
+    }
+  }
+  races
+}
+
+
+find_best_c <- function(c_vector, races_list){
+  # DF to store results at different levels of c
+  average_results <- data.frame(matrix(NA, nrow = length(c_vec), ncol = 1))
+  # Period incrementer for races
+  iterator <- 1
+  # iterate through each c
+  for (c in c_vec){
+    # reset period
+    period <- 1
+    # reset glicko and correlations to the 1st race
+    first_race <- races_list[[1]][2]
+    glicko <- glicko_ratings(first_race)
+    rankings_df <- updated_rankings(glicko)
+    # keep track of R^2 values
+    r2_tracker <- rep(NA, length(races_list))
+    
+    # iterate through each race in the series
+    for (race in races_list){
+      race_df <- race[[1]]
+      race_ovo <- race[[2]]
+      # adjust period for the glicko race objects
+      period <- period + 1
+      # pass in rotating value of c
+      update <- race_update(glicko, rankings_df, race_df, race_ovo, c)
+      # save results from the update for use in next iteration
+      glicko <- update[[1]]
+      rankings_df <- update[[2]]
+      r2_tracker[period-1] <- update[[4]]
+    }
+    # Find the average R2 at that level of C
+    average_results[iterator,1] <- r2_tracker %>% mean()
+    iterator <- iterator + 1
+  }
+  # find argmax for C for average R^2
+  results_summary <- tibble(c = c_vec, average_R_squared = average_results[[1]])
+  best_c <- results_summary  %>% arrange(desc(average_R_squared)) %>%
+    select(c) %>% head(1) %>% pull()
+  best_c
+}
+
+rankings_over_time <- function(rankings_list, gender){
+  results <- tibble("tau_glicko" = rankings_list[[1]], 
+                    "tau_itu" = rankings_list[[2]], 
+                    "r2_glicko" = rankings_list[[3]], 
+                    "r2_itu" = rankings_list[[4]], 
+                    year)
+  tidy_df <- results %>% gather("metric", "score", -year)
+  
+  # take data after year 2012 for plotting and summarizing
+  year_summaries <- tidy_df %>%
+    filter(year  > 2012)  %>%
+    group_by(year, metric) %>%
+    summarize(average_score = mean(score))
+  
+  # create and display plots
+  r2<- year_summaries %>% filter(metric == "r2_glicko" | metric == "r2_itu")
+  kendall <- year_summaries %>% filter(metric == "tau_glicko" | metric == "tau_itu")
+  
+  r2_plot<- ggplot(r2) + aes(x = year, y = average_score, colour = metric) +
+    geom_line() + coord_cartesian(ylim = c(0, 1)) +
+    labs(y = "Yearly average R2", 
+         title = paste("R2 comparisons between ITU rankings and glicko scores-",
+                       gender))
+  
+  kendall_plot <- ggplot(kendall) + aes(x = year, y = average_score, colour = metric) +
+    geom_line() + coord_cartesian(ylim = c(0, 1)) +
+    labs(y = "Yearly average Kendall's Tau", 
+         title = paste("Kendall's Tau comparisons between ITU rankings and glicko scores-",
+                       gender))
+  
+  grid.arrange(r2_plot, kendall_plot, nrow = 2)
+  
+}
+
+
+summary_stats <- function(rankings_list){
+  results <- tibble("tau_glicko" = rankings_list[[1]], 
+                    "tau_itu" = rankings_list[[2]], 
+                    "r2_glicko" = rankings_list[[3]], 
+                    "r2_itu" = rankings_list[[4]], 
+                    year)
+  tidy_df <- results %>% gather("metric", "score", -year) %>%
+    filter(year > 2012) %>%
+    group_by(metric) %>%
+    summarise(mean_metric = mean(score))
+  tidy_df
+}
+
+
